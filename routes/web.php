4fd33e259f1731a51039fce8cdfe4f2c0e69917c
EkorRisu <wayanfebrian05@gmail.com>
2025-02-04 07:05:45 +0700

    update codee

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,6 @@ Route::get('/', function () {
 // routes/web.php
 Auth::routes();  // Menyediakan semua rute otentikasi default (login, register, logout, dll.)
 
-Route::get('/', action: function () {
-    return view('layouts.welcome');
-});
-
-Route::get('/login', 'App\\Http\\Controllers\\Auth\\LoginController@showLoginForm')->name('login');
-Route::post('/login', 'App\\Http\\Controllers\\Auth\\LoginController@login');
-Route::post('/logout', 'App\\Http\\Controllers\\Auth\\LogoutController@signout')->name('logout');
-Route::get('/register', 'App\\Http\\Controllers\\Auth\\RegisterController@registration')->name('register');
-Route::post('/register', 'App\\Http\\Controllers\\Auth\\RegisterController@register');
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
